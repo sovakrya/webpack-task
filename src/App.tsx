0560af44401c59ee156d68ae02fba3d72e3c6522
sovaKrya <tyan_34ee@mail.ru>
2024-09-23 11:31:07 +0300

    add solution

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="main-container">
+      <h1 className="main-title">Я люблю мемных котиков</h1>
+      <div className="main-image"></div>
+    </div>
+  );
+}
